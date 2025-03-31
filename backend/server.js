c40542b76f72eb8cd6f3918411e26e27addeca34
RichardGaillet <gaillet.richard@gmail.com>
2025-03-31 10:53:44 +0200

    MongoDB setup.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express"
+import { connectDB } from "./config/db.js"
 
 const app = express()
 
 app.get("/", (req, res) => res.send("Server is ready!"))
 
-app.listen(5000, () => console.log("Server started at http://localhost:5000"))
+app.listen(5000, () => {
+  connectDB()
+  console.log("Server started at http://localhost:5000")
+})
